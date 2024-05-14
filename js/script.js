b5a9918f1399cd4daa44d34847296b1d7395a17a
Rodrigo Solla <sollarodrigo@gmail.com>
2024-05-14 19:40:30 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*
+let nombreIngresado = prompt("Ingrese su nombre");
+
+
+alert(nombreIngresado + " Ya se te nota el gym :D ");
+*/
+
+function saludar (){
+    console.log("hola a todos");
+}
+
+saludar();
+
+function pedirNombre(){
+    let ingresarNombre = prompt("ingrese su nombre");
+    alert(ingresarNombre + " ya se te nota el gym :D")
+}
+
+pedirNombre();
